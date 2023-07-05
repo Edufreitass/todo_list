713abfa542d93f962e9718b3b72b2d25822a01db
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-07-05 00:07:47 -0300

    feat(API): gerenciamento de estado

--- test/src/controllers/home_controller_test.dart
+++ test/src/controllers/home_controller_test.dart
@@ -4,15 +4,28 @@ import 'package:todo_list/src/controllers/home_controller.dart';
 import 'package:todo_list/src/models/todo_model.dart';
 import 'package:todo_list/src/repositories/todo_repository.dart';
 
-class TodoRepositoryMock extends Mock implements TodoRepository{}
+class TodoRepositoryMock extends Mock implements TodoRepository {}
 
 void main() {
   final repository = TodoRepositoryMock();
   final controller = HomeController(repository);
 
+  tearDown(() {
+    controller.state = HomeState.start;
+  });
+
   test('deve preencher variavel todos', () async {
     when(() => repository.fetchTodos()).thenAnswer((_) async => [TodoModel()]);
+    expect(controller.state, HomeState.start);
     await controller.start();
+    expect(controller.state, HomeState.success);
     expect(controller.todos.isNotEmpty, true);
+  });
+
+  test('deve modificar o estado para error se a requisição falhar', () async {
+    when(() => repository.fetchTodos()).thenThrow(Exception());
+    expect(controller.state, HomeState.start);
+    await controller.start();
+    expect(controller.state, HomeState.error);
   });
 }
